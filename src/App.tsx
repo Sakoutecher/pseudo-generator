c94921aede35d64f833895f271f9b0fa3bac6e7d
hcampos <hcampospro@gmail.com>
2023-05-23 16:21:03 +0200

    💄 Global style ended

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 //Librairies
 import { ReactElement } from 'react'
 
+//Components
+import { PseudoGenerator } from '#/PseudoGenerator'
+import { GithubButton } from '#/GithubButton'
+
 const App = (): ReactElement => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <div
+      style={{
+        backgroundColor: '#e5e5f7',
+        opacity: '0.8',
+        backgroundImage: 'radial-gradient(#444cf7 0.5px, #e5e5f7 0.5px)',
+        backgroundSize: '10px 10px',
+      }}
+      className='w-screen h-screen relative font-jetbrains flex justify-center items-center'
+    >
+      <PseudoGenerator />
+      <GithubButton href={`#`} />
+    </div>
+  )
 }
 
 export default App
---
+++ src/lib/components/PseudoGenerator/PseudoGenerator.tsx
@@ -0,0 +1,29 @@
+//Components
+import { Slider } from '#/Slider'
+import { Select } from '#/Select'
+import { Checkbox } from '#/Checkbox'
+import { Button } from '#/Button'
+import { Response } from '#/Response'
+
+export const PseudoGenerator = () => {
+  //TODO: Ask gpt for the pseudo generation
+  //TODO: Ask with options wich select the user
+  //TODO: Display all of the pseudos to the user
+
+  return (
+    <div className='bg-zinc-400 backdrop-blur-sm h-2/5 rounded flex justify-between items-center p-4 gap-12'>
+      <div className='w-60 h-full relative'>
+        <Slider />
+        <Select />
+        <div className='flex flex-col items-center justify-center my-4 gap-4'>
+          <Checkbox label='Fun' />
+          <Checkbox label='Badass' />
+        </div>
+        <Button label={`Generate`} />
+      </div>
+      <div className='w-60 h-full'>
+        <Response />
+      </div>
+    </div>
+  )
+}
