e89727c14ce8e1465ad893205a661f00fb3e371e
hcampos <hcampospro@gmail.com>
2023-05-23 16:35:57 +0200

    ✨ Dynamic options

--- src/lib/components/Select/Select.tsx
+++ src/lib/components/Select/Select.tsx
@@ -1,10 +1,21 @@
-export const Select = () => {
+//Librairies
+import { FC } from 'react'
+
+type SelectProps = {
+  options: string[]
+}
+
+export const Select: FC<SelectProps> = ({ options }) => {
   return (
     <select className='my-4 w-full py-2 px-2 bg-transparent border-2 border-zinc-300 rounded outline-none'>
-      <option value=''>Hello</option>
-      <option value=''>Hello</option>
-      <option value=''>Hello</option>
-      <option value=''>Hello</option>
+      <option value='' disabled selected>
+        Select one
+      </option>
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
     </select>
   )
 }
