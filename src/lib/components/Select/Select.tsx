9e7de741e1a75f5b4ba66816e2950868d42b0b95
hcampos <hcampospro@gmail.com>
2023-05-23 16:43:27 +0200

    ♻️ Use the custom hook useselect

--- src/lib/components/Select/Select.tsx
+++ src/lib/components/Select/Select.tsx
@@ -1,13 +1,25 @@
 //Librairies
 import { FC } from 'react'
 
+//Hooks
+import { useSelectStore } from '../../hooks/useSelect/'
+
 type SelectProps = {
   options: string[]
 }
 
 export const Select: FC<SelectProps> = ({ options }) => {
+  const setSelect = useSelectStore((state) => state.setSelect)
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelect(e.target.value)
+  }
+
   return (
-    <select className='my-4 w-full py-2 px-2 bg-transparent border-2 border-zinc-300 rounded outline-none'>
+    <select
+      className='my-4 w-full py-2 px-2 bg-transparent border-2 border-zinc-300 rounded outline-none'
+      onChange={handleChange}
+    >
       <option value='' disabled selected>
         Select one
       </option>
