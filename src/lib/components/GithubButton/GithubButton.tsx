d8077bdec8329f71953cf45a4dcadd64495e81ca
hcampos <hcampospro@gmail.com>
2023-05-23 15:09:42 +0200

    ✨ GithubButton component

---
+++ src/lib/components/GithubButton/GithubButton.tsx
@@ -0,0 +1,26 @@
+//Librairies
+import { FC } from 'react'
+
+//Logo
+import GitHubLogo from '~/github-mark.svg'
+
+type GithubButtonProps = {
+  href: string
+}
+
+export const GithubButton: FC<GithubButtonProps> = ({ href }) => {
+  return (
+    <span className='text-md absolute top-5 left-6 flex items-center justify-center gap-3 uppercase'>
+      Star the project on{' '}
+      <a
+        href='href'
+        target='_blank'
+        rel='noreferrer'
+        className='py-3 px-4 bg-zinc-400 rounded-md flex items-center justify-center gap-2 hover:bg-zinc-400 transition ease-linear'
+      >
+        <img className='w-6' src={GitHubLogo} />
+        Github
+      </a>
+    </span>
+  )
+}
