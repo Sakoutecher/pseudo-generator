fd186d72bec13d11cef2ca76489a0c1f23ddc7fc
hcampos <hcampospro@gmail.com>
2023-05-23 16:20:11 +0200

    ✨ Select component

---
+++ src/lib/components/Select/Select.tsx
@@ -0,0 +1,10 @@
+export const Select = () => {
+  return (
+    <select className='my-4 w-full py-2 px-2 bg-transparent border-2 border-zinc-300 rounded outline-none'>
+      <option value=''>Hello</option>
+      <option value=''>Hello</option>
+      <option value=''>Hello</option>
+      <option value=''>Hello</option>
+    </select>
+  )
+}
