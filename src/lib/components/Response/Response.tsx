66aba61a271e6e00647034ce008ee5b89e0dfb0e
hcampos <hcampospro@gmail.com>
2023-05-24 11:12:10 +0200

    💄 Better display

--- src/lib/components/Response/Response.tsx
+++ src/lib/components/Response/Response.tsx
@@ -7,8 +7,10 @@ type ResponseProps = {
 
 export const Response: FC<ResponseProps> = ({ response }) => {
   return (
-    <div className='h-full w-full bg-zinc-300 rounded py-2 px-4'>
-      {response}
+    <div className='h-full w-full bg-zinc-300 rounded py-2 px-4 overflow-y-scroll'>
+      {response.split('\n').map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
     </div>
   )
 }
